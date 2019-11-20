11f09182ed51cfc3771e2fa6f1225e073a3819c0
Yong Ha <rladydgk0303@naver.com>
2019-11-20 18:10:41 +0900

    지구 낮과 밤 설정완료

---
+++ Assets/Scripts/Earth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float val = Mathf.PingPong(Time.time, 1);
+        GetComponent<Renderer>().material.SetFloat("_TexValue", val);
+    }
+}
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    public float speed = 1;
+    //public float speed = 1;
     void Update()
     {
+        float speed = 1;
         transform.Rotate(new Vector3(0, speed, 0));
-        
     }
 }
